a1e2f122a2bcf972a4e1e1e59722b96264efe92a
Max Girkins <mgirkins@gmail.com>
2018-01-31 17:10:27 +0000

    updated colour sensing and driving code to make robot stop at a black finish line, minor refactoring, turning improved by using both motors in opposite directions. TODO: change colour sensing mode for better accuracy and tweak movement so it works better on all surfaces.

--- Python/Hardware/Test code/YARR3.py
+++ Python/Hardware/Test code/YARR3.py
@@ -12,20 +12,32 @@ c1 = ColorSensor('in2')
 c2 = ColorSensor('in3')
 
 # setting ,mode for color sensors  to return values 0-7
-c2.mode('COL-COLOR')
-c1.mode('COL-COLOR')
+c2.mode = 'COL-COLOR'
+c1.mode = 'COL-COLOR'
 
 colours = ['unknown', 'black', 'blue', 'green', 'yellow', 'red', 'white', 'brown']
 
-while colours[c1.value()] == 'unknown':
+def driveForward(time, speed):
+    rightM.run_timed(time_sp=time, speed_sp=speed)
+    leftM.run_timed(time_sp=time, speed_sp=speed)
+
+def turnRight90():
+    rightM.run_timed(time_sp=1500, speed_sp=-400)
+    leftM.run_timed(time_sp=1500, speed_sp=400)
+
+def turnLeft90():
+    rightM.run_timed(time_sp=1500, speed_sp=400)
+    leftM.run_timed(time_sp=1500, speed_sp=-400)
+
+while colours[c1.value()] == 'unknown' or  colours[c1.value()] == 'black':
     print("color not selected")
     time.sleep(1)
 
-chosen = c1.value()
+chosen = colours[c1.value()]
 time.sleep(1)
 
 print("colour choosen", chosen)
-Sound.speak(colours[c1.value()])
+Sound.speak(chosen)
 time.sleep(1)
 
 print("Motor A is connected:", m1.connected)
@@ -34,18 +46,16 @@ print("Color senor mode: ", c1.mode)
 # put camera arm camera arm down and
 m1.run_timed(time_sp=600, speed_sp=-200)
 time.sleep(1)
-leftM.run_timed(time_sp=5000, speed_sp=900)
-rightM.run_timed(time_sp=5000, speed_sp=900)
+driveForward(5000,500)
 
 # Stop when chosen colour is found by second colour sensor
-while not ((c2.value() == chosen) or (c2.value() == 'black')):
-    print("help!!")
-
+while not ((colours[c2.value()] == 'unknown') or (colours[c2.value()] == 'black')):
+    print(c2.value())
+print(colours[c2.value()])
 Sound.speak("Thank you")
 
 leftM.stop()
 rightM.stop()
 time.sleep(2)
+turnRight90()
 m1.run_timed(time_sp=600, speed_sp=200)
-time.sleep(2)
-leftM.run_timed(time_sp=1500, speed_sp=500)
