9eb835444420cfb5f9571cade087fb3ce72c5a64
Max Girkins <mgirkins@gmail.com>
2018-02-23 15:14:18 +0000

    switched to stepped turning based on x coordinate of pattern in camera field of view. should provide a smooth turn given some testing and tweaking with completed vision code.

--- Python/Software/force_vision.py
+++ Python/Software/force_vision.py
@@ -8,52 +8,55 @@ bottle is centered in the robot's field of view thus exerting the maximum 'force
 bottle until the ultrasonic sensor says that it is close enough to pick up the bottle.
 '''
 # !/usr/bin/env python3
-
+from ev3dev.ev3 import *
 from time import time, sleep
 import math
 import sys
 
-#leftM = LargeMotor('outC')
-#rightM = LargeMotor('outB')
+leftM = LargeMotor('outC')
+rightM = LargeMotor('outB')
 camera_aspect_width = 160
 # standard deviation changes the sharpness of the turn (needs tuning)
 #sd = 0.1
 def sigmoid(x):
   return (1 / (1 + math.exp(-(x)))) - 0.5
-
+'''
 def normal_distribution(x):
     x_centered = x-(camera_aspect_width/2)
     mean = 0
     y = (1/(sd*math.sqrt(2*math.pi)))*math.e*(-((x_centered-mean)*(x_centered-mean)/2*(sd*sd)))
     return y
-
+'''
 def force_on_robot(pattern_x_coord, pattern_size):
     # analogous with light intensity
     pattern_intensity = 1/pattern_size
     # model as normal distribution with zero mean to give direction robot should turn
     distance_from_center = math.fabs(pattern_x_coord-80)
-    
-    if distance_from_center == 0 : distance_from_center += 0.0001
+    if distance_from_center == 0 : distance_from_center += 0.000001
     force = pattern_intensity * (1/(distance_from_center))
     # make force positive if object on the right
     if (pattern_x_coord > camera_aspect_width/2): force = -(math.fabs(force))
     return force
 
+def sine_mode(x_coord):
+    if x_coord<=(camera_aspect_width/2):
+        motor_speed_boost = math.sin(4*(x_coord-24))+1.1
+    else:
+        motor_speed_boost = -math.sin(4*(x_coord-24))+1.1    
+    
+    return motor_speed_boost
+
 def drive(force):
     base_speed = 200
-    speed_rightM = 200
-    speed_leftM = 200
-    if (force > 0):
-        speed_leftM = base_speed - sigmoid(1/force)*100
-        speed_rightM = base_speed + sigmoid(1/force)*100     
-    else :
-        speed_leftM = base_speed + sigmoid(1/force)*100
-        speed_rightM = base_speed - sigmoid(1/force)*100
-    #rightM.run_timed(time_sp=time_, speed_sp=speed_rightM)
-    #leftM.run_timed(time_sp=time_, speed_sp=speed_leftM)
+    speed_leftM = base_speed - force*100
+    speed_rightM = base_speed + force*100     
+    #rightM.run_timed(time_sp=100, speed_sp=speed_rightM)
+    #leftM.run_timed(time_sp=100, speed_sp=speed_leftM)
     print("left motor speed: " + str(speed_leftM))
     print("right motor speed: " + str(speed_rightM))
 
-print(force_on_robot(float(sys.argv[1]),float(sys.argv[2])))
-drive(force_on_robot(float(sys.argv[1]),float(sys.argv[2])))
+#print(force_on_robot(float(sys.argv[1]),float(sys.argv[2])))
+
+for x in xrange(80,0):
+    drive(sine_mode(x))
 
