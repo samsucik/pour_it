c33096e8703d5b6bd338ab81980e88c556e08039
Max Girkins <mgirkins@gmail.com>
2018-02-23 15:50:04 +0000

    wrote methods to turn to face the bottle and stop when ultrasonic sensor is 5 cm from the bottle. the arc of the turn can be tuned as and the distance to the bottle needs to be tuned as well so that the gripper ends in the right place

--- Python/Software/turn_to_bottle.py
+++ Python/Software/turn_to_bottle.py
@@ -8,25 +8,29 @@ bottle is centered in the robot's field of view thus exerting the maximum 'force
 bottle until the ultrasonic sensor says that it is close enough to pick up the bottle.
 '''
 # !/usr/bin/env python3
-from ev3dev.ev3 import *
+#from ev3dev.ev3 import *
 from time import time, sleep
 import math
 import sys
+import json
 
-leftM = LargeMotor('outC')
-rightM = LargeMotor('outB')
+#leftM = LargeMotor('outC')
+#rightM = LargeMotor('outB')
 camera_aspect_width = 160
 # standard deviation changes the sharpness of the turn (needs tuning)
 #sd = 0.1
+leftm = []
+rightm = []
+'''
 def sigmoid(x):
   return (1 / (1 + math.exp(-(x)))) - 0.5
-'''
+
 def normal_distribution(x):
     x_centered = x-(camera_aspect_width/2)
     mean = 0
     y = (1/(sd*math.sqrt(2*math.pi)))*math.e*(-((x_centered-mean)*(x_centered-mean)/2*(sd*sd)))
     return y
-'''
+
 def force_on_robot(pattern_x_coord, pattern_size):
     # analogous with light intensity
     pattern_intensity = 1/pattern_size
@@ -37,26 +41,61 @@ def force_on_robot(pattern_x_coord, pattern_size):
     # make force positive if object on the right
     if (pattern_x_coord > camera_aspect_width/2): force = -(math.fabs(force))
     return force
-
 def sine_mode(x_coord):
     if x_coord<=(camera_aspect_width/2):
-        motor_speed_boost = math.sin(4*(x_coord-24))+1.1
+        motor_speed_boost = (math.sin(6*((x_coord/65)-17))+1)*50
     else:
-        motor_speed_boost = -math.sin(4*(x_coord-24))+1.1    
+        motor_speed_boost = (-math.sin(6*((x_coord/65)-17))+1)*50
     
     return motor_speed_boost
+'''
+#returns speed multiplier for the motor to ramp up
+def step_turn(x_coord):
+    proportion = float((x_coord/float(camera_aspect_width))*100)
+    print(x_coord)
+    print(proportion)
+    if (proportion >= 0) and (proportion < 10):
+        return -1*((10-proportion)/10)
+    elif (proportion >= 10) and (proportion < 20):
+        return -(1.0*((proportion-10)/10))
+    elif (proportion >= 20) and (proportion < 40):
+        return -1
+    elif (proportion >= 40) and (proportion < 50):
+        return -(1.0*((10-(proportion-40))/10))
+    elif (proportion >= 50) and (proportion < 60):
+        return (1.0*((proportion-50)/10))
+    elif (proportion >= 60) and (proportion < 80):
+        return 1
+    elif (proportion >= 80) and (proportion < 90):
+        return 1*((10-(proportion-80))/10)
+    elif (proportion >= 90) and (proportion <= 100):
+        return 1*((proportion-90)/10)
+    else:
+        return 0
 
 def drive(force):
-    base_speed = 200
-    speed_leftM = base_speed - force*100
-    speed_rightM = base_speed + force*100     
-    #rightM.run_timed(time_sp=100, speed_sp=speed_rightM)
-    #leftM.run_timed(time_sp=100, speed_sp=speed_leftM)
+    base_speed = 100
+    if force >= 0:
+        speed_leftM = base_speed + (force*100)
+        speed_rightM = base_speed 
+    else:
+        speed_leftM = base_speed 
+        speed_rightM = base_speed - (force*100)     
+         
+    #rightM.run_timed(time_sp=100, speed_sp=speed_rightM).wait()
+    #leftM.run_timed(time_sp=100, speed_sp=speed_leftM).wait()
+    leftm.append(speed_leftM)
+    rightm.append(speed_rightM)
     print("left motor speed: " + str(speed_leftM))
     print("right motor speed: " + str(speed_rightM))
 
 #print(force_on_robot(float(sys.argv[1]),float(sys.argv[2])))
 
-for x in xrange(80,0):
-    drive(sine_mode(x))
+for x in xrange(0,160):
+    drive(step_turn(x))
+movements = {
+    'rightM': rightm,
+    'leftM': leftm
+}
+print(json.dumps(movements))
 
