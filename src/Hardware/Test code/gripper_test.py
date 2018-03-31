a1daa7c0dafa0b61384201858743d244f65e2ef6
Sam Sucik <s1513472@wigton.inf.ed.ac.uk>
2018-03-31 01:53:14 +0100

    Modified gripper test. Added hard-coded WORKING demo for gripping, lifting, opening.

--- src/Hardware/Test code/gripper_test.py
+++ src/Hardware/Test code/gripper_test.py
@@ -1,9 +1,10 @@
 #remote python call setup
-import ev3dev.ev3 as ev3
+#import ev3dev.ev3 as ev3
 import rpyc
 conn = rpyc.classic.connect('ev3dev') #host name or IP address of EV3
 ev3 = conn.modules['ev3dev.ev3'] #import ev3dev.ev3
 import time
+from time import sleep
 
 gripper = ev3.MediumMotor("outA")
 touchSensor = ev3.TouchSensor()
@@ -54,13 +55,27 @@ def returnPourer():
     pourer.run_forever(speed_sp=50)
 
 def armExtend():
-    arm.run_forever(speed_sp=-200)
+    arm.run_forever(speed_sp=400)
 
 def armReturn():
-    arm.run_forever(speed_sp=200)
+    arm.run_forever(speed_sp=-600)
 
 def openerBottle():
     opener.run_forever(speed_sp=800)
+
+
+def lift_open_demo():
+	closeGripper()
+	sleep(4)
+	pourer.run_timed(speed_sp=-50, time_sp=11000)
+	sleep(11)
+	opener.run_timed(speed_sp=800, time_sp=(20000+12000))
+	arm.run_timed(speed_sp=400, time_sp=20000)
+	sleep(20+12)
+	pourer.run_timed(speed_sp=50, time_sp=5000)
+	sleep(5)
+	arm.run_timed(speed_sp=-600, time_sp=13400)
+
 
 # -------- Run section -------
 
@@ -68,37 +83,49 @@ def openerBottle():
 # time.sleep(3)
 # openGripper()
 btn = ev3.Button()
-
+start = time.time()
 # gripper.run_timed(speed_sp=200, time_sp=3000)
 ##### hit enter to stop pourer
 while True:
     k = input("input char: ")
+    time_new = time.time()
+    delta_t = time_new - start
+    print(delta_t)
+    start = time_new
     if k == "w":
         openGripper()
     elif k == "s":
         closeGripper()
+    
     elif k == "\x1b[A":
         armExtend()
         # driveForward(200)
     elif k == "\x1b[B":
         armReturn()
         # driveBackward(-200)
+    
     elif k == "\x1b[C":
         pass
         # turnRight90()
     elif k == "\x1b[D":
         pass
         # turnLeft90()
+    
     elif k == "p":
         startPouring()
     elif k == "l":
         returnPourer()
+    
     elif k == "g":
        gripper.stop()
+    
     elif k == "z":
         arm.stop()
+    
     elif k == "q":
         openerBottle()
+    elif k == "y":
+    	lift_open_demo()
     else:
         pourer.stop(stop_action="hold")
         opener.stop()
